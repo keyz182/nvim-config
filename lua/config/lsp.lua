b40fca6c63f076fbb85bc424bffe83a766a7b4df
Kieran David Evans <keyz182@gmail.com>
2025-10-16 08:56:19 +0100

    feat: add ruff as lsp

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,9 +1,25 @@
-vim.lsp.config('ruff', {
+vim.lsp.config("ruff", {
   init_options = {
     settings = {
       -- Ruff language server settings go here
-    }
-  }
+    },
+  },
 })
 
-vim.lsp.enable('ruff')
+vim.lsp.enable("ruff")
+
+vim.filetype.add({
+  pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = { "*.hl", "hypr*.conf" },
+  callback = function(event)
+    print(string.format("starting hyprls for %s", vim.inspect(event)))
+    vim.lsp.start({
+      name = "hyprlang",
+      cmd = { "hyprls" },
+      root_dir = vim.fn.getcwd(),
+    })
+  end,
+})
