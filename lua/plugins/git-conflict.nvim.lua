b51bb67928d4c8ea96287e20093ab28ce324b24c
Kieran David Evans <keyz182@gmail.com>
2025-10-16 09:35:14 +0100

    Cleanup plugins

--- lua/plugins/git-conflict.nvim.lua
+++
@@ -1,9 +0,0 @@
--- Plugin: akinsho/git-conflict.nvim
--- Installed via store.nvim
-
-return {
-    "akinsho/git-conflict.nvim",
-    version = "*",
-    config = true,
-    event = "VeryLazy"
-}
--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -101,6 +101,42 @@ return {
         -- tsserver will be automatically installed with mason and loaded with lspconfig
         tsserver = {},
         ruff = {},
+        yamlls = {
+          -- Have to add this for yamlls to understand that we support line folding
+          capabilities = {
+            textDocument = {
+              foldingRange = {
+                dynamicRegistration = false,
+                lineFoldingOnly = true,
+              },
+            },
+          },
+          -- lazy-load schemastore when needed
+          before_init = function(_, new_config)
+            new_config.settings.yaml.schemas = vim.tbl_deep_extend(
+              "force",
+              new_config.settings.yaml.schemas or {},
+              require("schemastore").yaml.schemas()
+            )
+          end,
+          settings = {
+            redhat = { telemetry = { enabled = false } },
+            yaml = {
+              keyOrdering = false,
+              format = {
+                enable = true,
+              },
+              validate = true,
+              schemaStore = {
+                -- Must disable built-in schemaStore support to use
+                -- schemas from SchemaStore.nvim plugin
+                enable = false,
+                -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+                url = "",
+              },
+            },
+          },
+        },
       },
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
@@ -162,5 +198,96 @@ return {
     dependencies = { "OXY2DEV/markview.nvim" },
     opts = {},
     cmd = "Store",
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      timeout = 3500,
+    },
+  },
+  {
+    "nvim-neotest/nvim-nio",
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    config = function()
+      require("dap-python").setup("/usr/local/bin/debugpy")
+    end,
+  },
+  {
+    "f-person/git-blame.nvim",
+    -- load the plugin at startup
+    event = "VeryLazy",
+    -- Because of the keys part, you will be lazy loading this plugin.
+    -- The plugin will only load once one of the keys is used.
+    -- If you want to load the plugin at startup, add something like event = "VeryLazy",
+    -- or lazy = false. One of both options will work.
+    opts = {
+      -- your configuration comes here
+      -- for example
+      enabled = true, -- if you want to enable the plugin
+      message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+      date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+      virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+    },
+  },
+  {
+    "akinsho/git-conflict.nvim",
+    version = "*",
+    config = true,
+    event = "VeryLazy",
+  },
+  { "diogo464/kubernetes.nvim", event = "VeryLazy" },
+  { "jay-babu/mason-nvim-dap.nvim", event = "VeryLazy" },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua", -- optional
+      "nvim-mini/mini.pick", -- optional
+      "folke/snacks.nvim", -- optional
+    },
+  },
+  {
+    "ryanoasis/vim-devicons",
+    event = "VeryLazy",
+  },
+  {
+    "mfussenegger/nvim-dap",
+    event = "VeryLazy",
+    config = function() end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    event = "VeryLazy",
+  },
+  {
+    "julianolf/nvim-dap-lldb",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      codelldb_path = "/path/to/codelldb",
+    },
+  },
+  {
+    "leoluz/nvim-dap-go",
+    event = "VeryLazy",
+  },
+  {
+    "nicholasmata/nvim-dap-cs",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
   },
 }
